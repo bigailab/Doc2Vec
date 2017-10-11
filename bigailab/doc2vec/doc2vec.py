4e28fdd11f3c8fcd8e76f85c5653df869dd5e8fe
Ozge AYAZ <ozge.ayaz@argela.com.tr>
2017-10-11 23:05:20 +0300

    edit stopwords  file  from nltk library

--- bigailab/doc2vec/doc2vec.py
+++ bigailab/doc2vec/doc2vec.py
@@ -1,30 +1,82 @@
-from gensim import models
+import gensim
+import os
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from gensim.models.doc2vec import TaggedDocument
+from nltk.corpus import stopwords
 
-class LabeledLineSentence(object):
-    def __init__(self, filename):
-        self.filename = filename
 
-    def __iter__(self):
-        for uid, line in enumerate(open(self.filename)):
-            yield LabeledLineSentence(words=line.split(), labels=['SENT_%s' % uid])
 
-#LabeledLineSentence('/home/ozgecan/beyazperde.com/usercomments/clean/user_clean_0.txt')
-sentence = LabeledLineSentence('/home/ozgecan/beyazperde.com/usercomments/clean/user_clean_0.txt', labels=["SENT_0"])
-sentence1 = LabeledLineSentence('/home/ozgecan/beyazperde.com/usercomments/clean/user_clean_0.txt', labels=["SENT_1"])
+def get_doc_list(folder_name):
+    doc_list = []
+    file_list = [folder_name + '/' + name for name in os.listdir(folder_name) if name.endswith('txt')]
+    for file in file_list:
+        st = open(file, 'r').read()
+        doc_list.append(st)
+    print('Found %s documents under the dir %s .....' % (len(file_list), folder_name))
+    return doc_list
 
-sentences = [sentence, sentence1]
-model = models.Doc2Vec(alpha=.025, min_alpha=.025, min_count=1)
-model.build_vocab(sentences)
 
-for epoch in range(10):
-    model.train(sentences)
-    model.alpha -= 0.002  # decrease the learning rate`
+def get_doc(folder_name):
+    doc_list = get_doc_list(folder_name)
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stop = stopwords.words('turkish')
+    p_stemmer = PorterStemmer()
+
+    taggeddoc = []
+
+    texts = []
+    for index, i in enumerate(doc_list):
+        # for tagged doc
+        wordslist = []
+        tagslist = []
+
+        # clean and tokenize document string
+        raw = i.lower()
+        tokens = tokenizer.tokenize(raw)
+
+        # remove stop words from tokens
+        stopped_tokens = [i for i in tokens if not i in en_stop]
+
+        # remove numbers
+        number_tokens = [re.sub(r'[\d]', ' ', i) for i in stopped_tokens]
+        number_tokens = ' '.join(number_tokens).split()
+
+        # stem tokens
+        stemmed_tokens = [p_stemmer.stem(i) for i in number_tokens]
+        # remove empty
+        length_tokens = [i for i in stemmed_tokens if len(i) > 1]
+        # add tokens to list
+        texts.append(length_tokens)
+
+        td = TaggedDocument(gensim.utils.to_unicode(str.encode(' '.join(stemmed_tokens))).split(), str(index))
+        taggeddoc.append(td)
+
+    return taggeddoc
+
+
+documents = get_doc('/home/ozgecan/beyazperde.com/usercomments/clean')
+print('Data Loading finished')
+
+print(len(documents), type(documents))
+
+# build the model
+model = gensim.models.Doc2Vec(documents, dm=0, alpha=0.025, size=20, min_alpha=0.025, min_count=0)
+
+# start training
+for epoch in range(200):
+    if epoch % 20 == 0:
+        print('Now training epoch %s' % epoch)
+    model.train(documents)
+    model.alpha -= 0.002  # decrease the learning rate
     model.min_alpha = model.alpha  # fix the learning rate, no decay
 
-model.save("/home/ozgecan/my_model.txt")
-model_loaded = models.Doc2Vec.load('/home/ozgecan/my_model.txt')
+# shows the similar words
+print(model.most_similar('suppli'))
 
-print
-model.docvecs.most_similar(["SENT_0"])
-print
-model_loaded.docvecs.most_similar(["SENT_1"])
+# shows the learnt embedding
+print(model['suppli'])
+
+# shows the similar docs with id = 2
+print(model.docvecs.most_similar(str(2)))
--- bigailab/word2vec/word2vec.py
+++ bigailab/word2vec/word2vec.py
@@ -52,8 +52,3 @@ class Test():
 Test()
 
 
-
-
-
-
-
